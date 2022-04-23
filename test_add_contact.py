0aa4495f8ea04773b3a07a13cf68c70898b98eee
Alexander Polyansky <alexander@postinbox.com>
2022-04-23 17:23:10 +0300

    Add test for adding a contact from Katalon Recorder

---
+++ test_add_contact.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+
+    def test_add_contact(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+        driver.find_element_by_link_text("add new").click()
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys("George")
+        driver.find_element_by_name("middlename").click()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys("Jr.")
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys("Plush")
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("nickname").send_keys("Plushie")
+        driver.find_element_by_name("title").click()
+        driver.find_element_by_name("title").clear()
+        driver.find_element_by_name("title").send_keys("Mr.")
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys("Bright House")
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("address").send_keys(
+            "Bright House, 1500 Transylvania Avenue, S.E., Washington, DC 20500")
+        driver.find_element_by_name("home").click()
+        driver.find_element_by_name("home").clear()
+        driver.find_element_by_name("home").send_keys("555-123-4567")
+        driver.find_element_by_name("mobile").click()
+        driver.find_element_by_name("mobile").clear()
+        driver.find_element_by_name("mobile").send_keys("555-234-5678")
+        driver.find_element_by_name("work").click()
+        driver.find_element_by_name("work").clear()
+        driver.find_element_by_name("work").send_keys("555-345-6789")
+        driver.find_element_by_name("fax").click()
+        driver.find_element_by_name("fax").clear()
+        driver.find_element_by_name("fax").send_keys("555-456-7890")
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("contact@brighthouse.qa")
+        driver.find_element_by_name("email2").click()
+        driver.find_element_by_name("email2").clear()
+        driver.find_element_by_name("email2").send_keys("contact2@brighthouse.qa")
+        driver.find_element_by_name("email3").click()
+        driver.find_element_by_name("email3").clear()
+        driver.find_element_by_name("email3").send_keys("contact3@brighthouse.qa")
+        driver.find_element_by_name("homepage").click()
+        driver.find_element_by_name("homepage").clear()
+        driver.find_element_by_name("homepage").send_keys("https://www.brighthouse.qa")
+        driver.find_element_by_name("bday").click()
+        Select(driver.find_element_by_name("bday")).select_by_visible_text("23")
+        driver.find_element_by_xpath("//option[@value='23']").click()
+        driver.find_element_by_name("bmonth").click()
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("April")
+        driver.find_element_by_xpath("//option[@value='April']").click()
+        driver.find_element_by_name("byear").click()
+        driver.find_element_by_name("byear").clear()
+        driver.find_element_by_name("byear").send_keys("2002")
+        driver.find_element_by_name("aday").click()
+        Select(driver.find_element_by_name("aday")).select_by_visible_text("23")
+        driver.find_element_by_xpath("//div[@id='content']/form/select[3]/option[25]").click()
+        driver.find_element_by_name("amonth").click()
+        Select(driver.find_element_by_name("amonth")).select_by_visible_text("April")
+        driver.find_element_by_xpath("//div[@id='content']/form/select[4]/option[5]").click()
+        driver.find_element_by_name("ayear").click()
+        driver.find_element_by_name("ayear").clear()
+        driver.find_element_by_name("ayear").send_keys("2012")
+        driver.find_element_by_name("address2").click()
+        driver.find_element_by_name("address2").clear()
+        driver.find_element_by_name("address2").send_keys(
+            "QA Department of Python, Office of Inspector General, DEV-62, 1800 Moore Law Street, Arlington, VA 22209")
+        driver.find_element_by_name("phone2").click()
+        driver.find_element_by_name("phone2").clear()
+        driver.find_element_by_name("phone2").send_keys("555-098-1234")
+        driver.find_element_by_name("notes").click()
+        driver.find_element_by_name("notes").clear()
+        driver.find_element_by_name("notes").send_keys(
+            "This contact was created by an automated Python test! ")
+        driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        driver.find_element_by_link_text("home page").click()
+        driver.find_element_by_link_text("Logout").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
