94b368496cb58dd42d2cb5d4de9e525d61cbdcaa
Alexander Polyansky <alexander@postinbox.com>
2022-04-23 14:27:41 +0300

    Add comments to the test

--- test_add_group.py
+++ test_add_group.py
@@ -11,7 +11,9 @@ class test_add_group(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # log in
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -19,8 +21,11 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page
         wd.find_element_by_link_text("groups").click()
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("Test")
@@ -30,8 +35,11 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("Test")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        # return to groups page
         wd.find_element_by_link_text("group page").click()
+        # log out
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
