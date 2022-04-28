58015bf22b1b558255891d9c7f1a4a360e740ae0
Alexander Polyansky <alexander@postinbox.com>
2022-04-28 23:05:07 +0300

    Add method to get a filled contact in the contact test

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,37 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+
+from contact import Contact
+
 import unittest
+
+def get_filled_contact():
+    contact = Contact()
+    contact.first_name = "George"
+    contact.middle_name = "Jr."
+    contact.last_name = "Plush"
+    contact.nickname = "Plushie"
+    contact.title = "Mr."
+    contact.company_name = "Bright House"
+    contact.address = "Bright House, 1500 Transylvania Avenue, S.E., Washington, DC 20500"
+    contact.home_phone = "555-123-4567"
+    contact.mobile_phone = "555-234-5678"
+    contact.work_phone = "555-345-6789"
+    contact.fax_phone = "555-456-7890"
+    contact.email = "contact@brighthouse.qa"
+    contact.email_2 = "contact2@brighthouse.qa"
+    contact.email_3 = "contact3@brighthouse.qa"
+    contact.homepage = "https://www.brighthouse.qa"
+    contact.birthday = datetime.date
+    contact.anniversary = datetime.date(year=2012, month=12, day=1)
+    contact.address_secondary = \
+        "QA Department of Python, Office of Inspector General, DEV-62, 1800 Moore Law Street, Arlington, VA 22209"
+    contact.home_phone_secondary = "555-098-1234"
+    contact.notes = "This contact was created by an automated Python test!"
+    return contact
 
 class test_add_contact(unittest.TestCase):
     def setUp(self):
