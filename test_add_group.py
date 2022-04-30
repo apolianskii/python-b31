e40067da2409537e77759e153ddba1a6788f1251
Alexander Polyansky <alexander@postinbox.com>
2022-04-30 16:18:36 +0300

    Remove WebDriver calls from test methods

--- test_add_group.py
+++ test_add_group.py
@@ -12,22 +12,22 @@ class test_add_group(unittest.TestCase):
         self.wd.implicitly_wait(30)
     
     def test_add_group(self):
-        wd = self.wd
-        self.log_in(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="Test", header="Test", footer="Test"))
-        self.log_out(wd)
+        self.log_in(username="admin", password="secret")
+        self.create_group(Group(name="Test", header="Test", footer="Test"))
+        self.log_out()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.log_in(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.log_out(wd)
+        self.log_in(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=""))
+        self.log_out()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def log_in(self, wd, username, password):
-        self.open_home_page(wd)
+    def log_in(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -36,17 +36,21 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def log_out(self, wd):
+    def log_out(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def open_groups_page(self, wd):
+    def open_groups_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def open_groups_page_after_creation(self, wd):
+    def open_groups_page_after_creation(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, group):
-        self.open_groups_page(wd)
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -62,7 +66,7 @@ class test_add_group(unittest.TestCase):
         # submit group creation
         wd.find_element_by_name("submit").click()
         # return to groups page
-        self.open_groups_page_after_creation(wd)
+        self.open_groups_page_after_creation()
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
