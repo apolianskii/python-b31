289449b040bd6b42a4b55e133f8335752eff9f14
Alexander Polyansky <alexander@postinbox.com>
2022-04-30 16:35:47 +0300

    Replace unittest with pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from application import Application
 from group import Group
 
-import unittest
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-    
-    def test_add_group(self):
-        self.app.log_in(username="admin", password="secret")
-        self.app.create_group(Group(name="Test", header="Test", footer="Test"))
-        self.app.log_out()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_group(self):
-        self.app.log_in(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.log_out()
-    
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.log_in(username="admin", password="secret")
+    app.create_group(Group(name="Test", header="Test", footer="Test"))
+    app.log_out()
+
+
+def test_add_empty_group(app):
+    app.log_in(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.log_out()
