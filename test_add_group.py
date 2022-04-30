4872fe4be5a5c178fc59e613da6ef0bd8672be7d
Alexander Polyansky <alexander@postinbox.com>
2022-04-30 16:09:09 +0300

    Remove low-level page methods from group tests

--- test_add_group.py
+++ test_add_group.py
@@ -13,26 +13,21 @@ class test_add_group(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.log_in(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="Test", header="Test", footer="Test"))
-        self.open_groups_page_after_creation(wd)
         self.log_out(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.log_in(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.open_groups_page_after_creation(wd)
         self.log_out(wd)
 
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
     def log_in(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -51,6 +46,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -65,6 +61,8 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        # return to groups page
+        self.open_groups_page_after_creation(wd)
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
